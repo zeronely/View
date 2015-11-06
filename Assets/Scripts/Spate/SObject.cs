271fab421b537acf7d4ac307226c9da752f118cc
zeronely <523532274@qq.com>
2015-11-06 22:23:46 +0800

    View类创建

---
+++ Assets/Scripts/Spate/SObject.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[SerializeField]
+public class SObject
+{
+    public string Name;
+    public GameObject Obj;
+
+    public SObject(string name,GameObject obj)
+    {
+        Name = name;
+        Obj = obj;
+    }
+}
--- Assets/Scripts/Spate/View.cs
+++ Assets/Scripts/Spate/View.cs
@@ -4,13 +4,14 @@ using System.Collections;
 public class View : MonoBehaviour
 {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
